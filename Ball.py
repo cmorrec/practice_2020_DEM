47edf094e4d82add7853c8923c12e879ae0c5239
Artem <aa_tema@mail.ru>
2020-07-11 18:10:44 +0300

    randomPolygon

--- Ball.py
+++ Ball.py
@@ -1,59 +1,122 @@
 from math import *
+from Wall import *
 
 
 class Ball:
     def __init__(self, x, y, canvas, color, radius, alpha, velocity):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.velocityAbsolute = velocity
         self.alphaRadian = alpha * pi / 180
         self.velocityX = velocity * cos(self.alphaRadian)
         self.velocityY = velocity * sin(self.alphaRadian)
-        self.radius = radius
-        self.x = x
-        self.y = y
-        self.velocityAbsolute = velocity
         self.canvas = canvas
         self.id = canvas.create_oval(x - radius, y - radius, x + radius, y + radius, fill=color)
-        self.canvas_height = self.canvas.winfo_height()
-        self.canvas_width = self.canvas.winfo_width()
         self.starts = False
         self.started = False
-        self.canvas.bind_all('<KeyPress-s>', self.start)                # s - начало движения
-        self.canvas.bind_all('<KeyPress-e>', self.exit)                 # e - конец движения
-
-    def move(self):
-        pos = self.canvas.coords(self.id)                               # овал задается по 4-м коордиатам по которым
-        self.x = (pos[0] + pos[2]) / 2                                  # можно найти координаты центра
-        self.y = (pos[1] + pos[3]) / 2
-
-        if not self.isInside():
-            self.reset()
-            self.velocityX = self.velocityAbsolute * cos(self.alphaRadian)
-            self.velocityY = self.velocityAbsolute * sin(self.alphaRadian)
-
-    def reset(self):
-        # смена углов в зависимости от стены от которой оттолкнулся мяч
-        if self.x < self.radius or self.x + self.radius > self.canvas_width:
-            if self.alphaRadian < 0:
-                self.alphaRadian = -pi - self.alphaRadian
-            else:
-                self.alphaRadian = pi - self.alphaRadian
-        elif self.y < self.radius or self.y + self.radius > self.canvas_height:
-            if (((self.y < self.radius) and (sin(self.alphaRadian) < 0)) or
-                    ((self.y + self.radius > self.canvas_height) and (sin(self.alphaRadian) > 0))):
-                # последнее сложное условие необходимо для того чтобы мяч успевал отойти от стены
-                # при некоторых углах он бы без этого условия двигался бы вдоль стены
-                self.alphaRadian *= -1
-
-    def isInside(self):
-        # проверка на выход мяча из границ
-        return (self.x >= self.radius) and (self.x + self.radius <= self.canvas_width) and \
-               (self.y >= self.radius) and (self.y + self.radius <= self.canvas_height)
-
-    def draw(self):
-        self.move()                                                 # фактическое движение
-        self.canvas.move(self.id, self.velocityX, self.velocityY)   # прорисовка движения
+        self.canvas.bind_all('<KeyPress-s>', self.start)  # s - начало движения
+        self.canvas.bind_all('<KeyPress-e>', self.exit)  # e - конец движения
 
     def start(self, event):
         self.started = True
 
     def exit(self, event):
         self.started = False
+
+    def drawPolygon(self, wall):
+        self.movePolygon(wall)  # фактическое движение
+        self.canvas.move(self.id, self.velocityX, self.velocityY)  # прорисовка движения
+
+    def movePolygon(self, wall):
+        pos = self.canvas.coords(self.id)  # овал задается по 4-м коордиатам по которым
+        self.x = (pos[0] + pos[2]) / 2  # можно найти координаты центра
+        self.y = (pos[1] + pos[3]) / 2
+
+        # Смена направления происходит в двух случаях(для обоих разные последствия):
+        #   - Пересечения мячом линии стенки
+        #   - Выхода за границы стенки(скорость больше радиуса * 2)
+        if self.crossPolygon(wall):
+            self.resetPolygon(wall)
+        elif not self.isInsidePolygon(wall):
+            self.comeBack(wall)
+        # Обновление направлений скоростей
+        self.velocityX = self.velocityAbsolute * cos(self.alphaRadian)
+        self.velocityY = self.velocityAbsolute * sin(self.alphaRadian)
+
+    def crossPolygon(self, wall):
+        # Проверяет пересечение как минимум с одной линией
+        for line in wall.lines:
+            if line.crossLine(self.x, self.y, self.radius):
+                return True
+        return False
+
+    def resetPolygon(self, wall):
+        # Находим линию, которую пересекает шарик и изменяем угол шарика по известной формуле:
+        # alpha = 2 * beta - alpha
+        for line in wall.lines:
+            if line.crossLine(self.x, self.y, self.radius):
+                # Проверка на то двигается ли мячик к линии или от нее
+                # (во втором случае менять направление не нужно)
+                if self.resetForLine(line, wall):
+                    self.alphaRadian = 2 * line.alphaTau - self.alphaRadian
+                    return
+
+    def resetForLine(self, line, wall):
+        # Проверяем расстояние сейчас и в следующий момент времени
+        # (с учетом нахождения внутри стен)
+        # Если сейчас расстояние больше - мячик двигается к стенке
+        distNow = line.distanceToLine(self.x, self.y)
+        if not self.isInsidePolygon(wall):
+            distNow *= -1
+
+        self.x += self.velocityX
+        self.y += self.velocityY
+        distAfter = line.distanceToLine(self.x, self.y)
+        if not self.isInsidePolygon(wall):
+            distAfter *= -1
+        self.x -= self.velocityX
+        self.y -= self.velocityY
+
+        return distNow > distAfter
+
+    def isInsidePolygon(self, wall):
+        # Направляем луч из центра шарика вертикально вверх и считаем количество пересечений с линиями стенки
+        # Если количество пересечений кратно двум, значит мяч вышел за границу стенки
+        summary = 0
+        for line in wall.lines:
+            if line.crossVerticalUp(self.x, self.y):
+                summary += 1
+        if summary % 2 == 1:
+            return True
+        else:
+            return False
+
+    def comeBack(self, wall):
+        # Возвращаем мяч в стенки в случае его выброса:
+        # Находим линию к которой мячу будет логичнее всего стремиться
+        # (меньше всего расстояние и перпендекуляр попадает в линию(в отрезок линии))
+        # и меняем направление в соответсвие с этой линией
+        eps = 1e-5  # eps необходимо поместить в одно место(второе в Line.py)
+        distances = []
+        for line in wall.lines:
+            distances.append(line.distanceToLine(self.x, self.y))
+        minDistance = min(distances)
+        key = True
+
+        while key:
+            for line in wall.lines:
+                if abs(line.distanceToLine(self.x, self.y) - minDistance) < eps:
+                    h = line.distanceToLine(self.x, self.y)
+                    xH = self.x + h * cos(pi - line.alphaNorm)
+                    yH = self.y - h * sin(pi - line.alphaNorm)
+                    if line.isLine(xH, yH) or len(distances) == 1:
+                        key = False
+                    else:
+                        distances.remove(minDistance)
+                        minDistance = min(distances)
+
+        for line in wall.lines:
+            if abs(line.distanceToLine(self.x, self.y) - minDistance) < eps:
+                self.alphaRadian = 2 * line.alphaTau - self.alphaRadian
+                return
--- DEM.py
+++ DEM.py
@@ -1,19 +1,23 @@
 from tkinter import *
 from Ball import *
+from Coordinate import *
+from Line import *
+from Wall import *
+import numpy as np
 import time
 
 mWidth = 500
 mHeight = 500
 
-x = 70
+x = 260
 print("Координата х шара в начальный момент времени: ", x)
-y = 50
+y = 240
 print("Координата y шара в начальный момент времени: ", y)
-velocity = 10
+velocity = 15
 print("Скорость шара равна: ", velocity)
 radius = 20
 print("Радиус шара равен: ", radius)
-alpha = 35
+alpha = -30
 print("Поворот вектора скорости относительно горизонтали: ", alpha)
 
 tk = Tk()
@@ -24,10 +28,43 @@ canvas = Canvas(tk, width=mWidth, height=mHeight, highlightthickness=0)
 canvas.pack()
 tk.update()
 
+# Класс Wall может принимать в себя любое количество линий(4 - это для примера)
+# Для большего количества точек(и соотвественно линий), надо создать их дополнительно и добавить в массив
+# При желании посмотреть на другие фигуры раскомментируйте(и закомментируйте) соотвествующие участки кода
+
+# Ромб
+coordinate1 = Coordinate(mWidth / 2, 0)
+coordinate2 = Coordinate(mWidth, mHeight / 2)
+coordinate3 = Coordinate(mWidth / 2, mHeight)
+coordinate4 = Coordinate(0, mHeight / 2)
+
+# Прямоугольник
+# coordinate1 = Coordinate(0, 0)
+# coordinate2 = Coordinate(mWidth, 0)
+# coordinate3 = Coordinate(mWidth, mHeight)
+# coordinate4 = Coordinate(0, mHeight)
+
+# Трапеция
+# coordinate1 = Coordinate(mWidth / 4, 0)
+# coordinate2 = Coordinate(3 * mWidth / 4, 0)
+# coordinate3 = Coordinate(mWidth, mHeight)
+# coordinate4 = Coordinate(0, mHeight)
+
+coordinates = np.array([coordinate1, coordinate2, coordinate3, coordinate3])
+
+line1 = Line(coordinate1.x, coordinate1.y, coordinate2.x, coordinate2.y)
+line2 = Line(coordinate2.x, coordinate2.y, coordinate3.x, coordinate3.y)
+line3 = Line(coordinate3.x, coordinate3.y, coordinate4.x, coordinate4.y)
+line4 = Line(coordinate4.x, coordinate4.y, coordinate1.x, coordinate1.y)
+lines = np.array([line1, line2, line3, line4])
+
+wall = Wall(coordinates, lines, canvas, 'black')
+
 ball = Ball(x, y, canvas, 'red', radius, alpha, velocity)
+
 while not ball.starts:
     if ball.started:
-        ball.draw()
+        ball.drawPolygon(wall)
     tk.update_idletasks()
     tk.update()
     time.sleep(0.1)
