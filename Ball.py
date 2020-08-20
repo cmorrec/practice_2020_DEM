b7666083438cced7bf822a2572c9f67c93079e69
Artem <aa_tema@mail.ru>
2020-08-20 12:03:35 +0300

    correct

--- Ball.py
+++ Ball.py
@@ -79,9 +79,8 @@ class Ball:
                     velocityXLocalWall = wall.velocityAbsolute * cos(alphaRadianLocal)
                     velocityYLocalWall = wall.velocityAbsolute * sin(alphaRadianLocal)
                     velocityXLocal += velocityXLocalWall
-                    velocityYLocal += velocityYLocalWall
                     dampeningNormal = velocityXLocal * cn_wall
-                    dampeningTangent = velocityYLocal * cs_wall
+                    dampeningTangent = (velocityYLocal - velocityYLocalWall) * cs_wall
                     self.velocityTheta += 1 / self.radius * (1 - cs_wall) * (
                             velocityYLocal - velocityYLocalWall - (self.velocityTheta * self.radius))
                     velocityXLocalNew = dampeningVelocity(dampeningNormal, velocityXLocal)
@@ -173,46 +172,3 @@ class Ball:
     def addAcceleration(self):
         self.accelerationX = MoveWall.getInstance().accelerationX
         self.accelerationY = MoveWall.getInstance().accelerationY
-
-    def methodWall(self, wall):
-        # Решение задачи о нецентральном упругом ударе двух дисков, путём приведения к задаче о
-        # столкновении шаров по оси Х(линия столкновения становится горизонтальной, происходит
-        # переход в локальную систему координат)
-        # Также учет диссипации при каждом столкновении шаров
-
-        # Угол между линией удара и горизонталью
-        gamma = atan2((i.y - j.y), (i.x - j.x))
-        # Углы направления шаров в локальной системе координат
-        alphaRadian1Local = i.alphaRadian - gamma
-        alphaRadian2Local = j.alphaRadian - gamma
-        # Скорости шаров в локальной системе координат
-        velocity1XLocal = i.velocityAbsolute * cos(alphaRadian1Local)
-        velocity1YLocal = i.velocityAbsolute * sin(alphaRadian1Local)
-        velocity2XLocal = j.velocityAbsolute * cos(alphaRadian2Local)
-        velocity2YLocal = j.velocityAbsolute * sin(alphaRadian2Local)
-
-        # Непосредственно решение задачи о нецентральном упругом ударе двух дисков
-        velocity1XLocalNew = ((i.mass - j.mass) * velocity1XLocal + 2 * j.mass * velocity2XLocal) / (i.mass + j.mass)
-        velocity2XLocalNew = (2 * i.mass * velocity1XLocal + (j.mass - i.mass) * velocity2XLocal) / (i.mass + j.mass)
-        # Демпфирование
-        dampeningNormalI = (velocity1XLocalNew - velocity2XLocalNew) * i.cn
-        dampeningNormalJ = (velocity2XLocalNew - velocity1XLocalNew) * j.cn
-        dampeningTangentI = (velocity1YLocal - velocity2YLocal - (
-                    i.velocityTheta * i.radius + j.velocityTheta * j.radius)) * i.cs
-        dampeningTangentJ = (velocity2YLocal - velocity1YLocal - (
-                    i.velocityTheta * i.radius + j.velocityTheta * j.radius)) * j.cs
-        # Учет демпфирования
-        velocity1XLocalNew = dampeningVelocity(dampeningNormalI, velocity1XLocalNew)
-        velocity2XLocalNew = dampeningVelocity(dampeningNormalJ, velocity2XLocalNew)
-        velocity1YLocal = dampeningVelocity(dampeningTangentI, velocity1YLocal)
-        velocity2YLocal = dampeningVelocity(dampeningTangentJ, velocity2YLocal)
-        # Задание новой угловой скорости дисков
-        rotation(i, j, velocity1YLocal, velocity2YLocal)
-        # Возвращение к глобальной системе координат
-        newAlphaI = atan2(velocity1YLocal, velocity1XLocalNew + eps) + gamma
-        newAlphaJ = atan2(velocity2YLocal, velocity2XLocalNew + eps) + gamma
-        newVelocityAbsoluteI = sqrt(velocity1XLocalNew ** 2 + velocity1YLocal ** 2)
-        newVelocityAbsoluteJ = sqrt(velocity2XLocalNew ** 2 + velocity2YLocal ** 2)
-        # Задание нового вектора скорости
-        i.changeVelocity(newAlphaI, newVelocityAbsoluteI)
-        j.changeVelocity(newAlphaJ, newVelocityAbsoluteJ)
